2099cae227bfde1b3b7ad34fe6817541dbfb9a03
Yaxye23 <125702106+Yaxye23@users.noreply.github.com>
2023-07-17 15:03:19 -0700

    commited the start method.

--- src/TriviaGame.java
+++ src/TriviaGame.java
@@ -9,6 +9,16 @@ public class TriviaGame {
     private int score;
     private Question currentQuestion;
     private List<Question> questions;
+    public void startGame(){
+        //read questions from file
+        questions= questionReader(questions.text);
+        //this is a method that reads the questions from a file
+
+        //create a list of questions
+        //shuffle the list
+        //set score to 0
+        //set current question to first question in list
+    }
 
     public void displayQuestion() {
         if (currentQuestion != null) {

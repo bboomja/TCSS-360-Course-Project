72c56b4df1ce995d8619627f678f9cf5bcecd00c
bboomja <gilrieom@icloud.com>
2023-07-17 15:41:22 -0700

    Implement updateScore() method and getUserChoice() helper method

--- src/TriviaGame.java
+++ src/TriviaGame.java
@@ -34,9 +34,42 @@ public class TriviaGame {
         }
     }
 
+    /**
+     * Updates the player's score based on the correctness of their answer.
+     * Retrieves the user's answer choice and compares it with the correct answer
+     * stored in the current question.
+     * If the user's answer is correct, the score is incremented by 1.
+     * Otherwise, the score remains unchanged.
+     * The updated score is displayed to the player.
+     */
     public void updateScore() {
+        int userChoice = getUserChoice();
 
+        if (currentQuestion != null) {
+            boolean isCorrect = currentQuestion.checkAnswer(userChoice);
+
+            if (isCorrect) {
+                score ++; // Increment the score if the user's answer is correct
+                System.out.println("Correct! Your score is now: " + score);
+            } else {
+                System.out.println("Incorrect! Yore score remains: " + score );
+            }
+        } else {
+            System.out.println("No question available.");
+        }
     }
 
+    /**
+     * Helper method.
+     * Retrieves the user's answer choice from the console.
+     *
+     * @return The user's answer choice as an integer
+     */
+    private int getUserChoice() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your answer choice: ");
+        int choice = sc.nextInt();
+        return choice;
+    }
 
 }

adfcdc910f195983e2a860ee3d9835964fda88d0
yareAli <sali8@uw.edu>
2023-07-19 17:03:08 -0700

    Implemented Theme Class and  includes all the methods to set and get the background and quiz topic.

--- src/model/Theme.java
+++ src/model/Theme.java
@@ -1,4 +1,27 @@
 package src.model;
 
 public class Theme {
+    private String background;
+    private String quizTopic;
+
+    public Theme(String background, String quizTopic) {
+        this.background = background;
+        this.quizTopic = quizTopic;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+
+    public void setQuizTopic(String quizTopic) {
+        this.quizTopic = quizTopic;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public String getQuizTopic() {
+        return quizTopic;
+    }
 }

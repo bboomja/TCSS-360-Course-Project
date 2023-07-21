a13434c7e501efa66d806b8a911bed7c41a69a0c
bboomja <gilrieom@icloud.com>
2023-07-21 15:44:14 -0700

    Implemented Maze class but not done yet.

--- src/model/Maze.java
+++ src/model/Maze.java
@@ -1,4 +1,113 @@
 package src.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Maze class represents the main game environment where the player navigates.
+ */
 public class Maze {
+    private Map map;
+    private Player player;
+    private Theme theme;
+    private List<Room> rooms;
+
+    /**
+     * Constructor for the Maze class. Initializes the list of rooms.
+     */
+    public Maze() {
+        this.rooms = new ArrayList<>();
+    }
+
+    /**
+     * Sets the map for the maze.
+     *
+     * @param map The map to be used in the maze.
+     */
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    /**
+     * Sets the player for the maze.
+     *
+     * @param player The player navigating the maze.
+     */
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    /**
+     * Sets the theme of the maze.
+     *
+     * @param theme The theme to be used in the maze.
+     */
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+
+    /**
+     * Moves the player in the specified direction.
+     *
+     * @param direction The direction in which to move the player.
+     */
+    public void movePlayer(String direction) {
+        // Implement logic for moving player in the given direction
+    }
+
+    /**
+     * Checks if a position in the maze is a wall.
+     *
+     * @param position The position to check.
+     * @return True if the position is a wall, false otherwise.
+     */
+    public boolean isWall(Position position) {
+        // Implement logic to check if a position is a wall
+        return false; // Placeholder return
+    }
+
+    /**
+     * Checks if a position in the maze is an exit.
+     *
+     * @param position The position to check.
+     * @return True if the position is an exit, false otherwise.
+     */
+    public boolean isExit(Position position) {
+        // Implement logic to check if a position is an exit
+        return false; // Placeholder return
+    }
+
+    /**
+     * Displays the current state of the maze.
+     */
+    public void displayMaze() {
+        // Implement logic for displaying the maze
+    }
+
+    /**
+     * Adds a room to the maze.
+     *
+     * @param room The room to be added.
+     */
+    public void addRoom(Room room) {
+        this.rooms.add(room);
+    }
+
+    /**
+     * Removes a room from the maze.
+     *
+     * @param room The room to be removed.
+     */
+    public void removeRoom(Room room) {
+        this.rooms.remove(room);
+    }
+
+    /**
+     * Gets the list of rooms in the maze.
+     *
+     * @return A list of rooms in the maze.
+     */
+    public List<Room> getRooms() {
+        return this.rooms;
+    }
 }
